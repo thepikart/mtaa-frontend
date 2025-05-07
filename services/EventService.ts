905213705b61d8950e4003b171a92bce33bf6c3b
Jakub-Nemec123 <jakub123.nemec@azet.sk>
2025-05-07 02:52:36 +0200

    event screen

--- services/EventService.ts
+++ services/EventService.ts
@@ -64,6 +64,45 @@ class EventService {
         const response = await api.get("/events/upcoming");
         return response.data;
       }
+
+        async searchEvents(query: string) {
+        const response = await api.get("/events/search", { params: { q: query } });
+        return response.data;
+    }
+
+    async getComments(eventId: number, limit = 20, offset = 0) {
+        const { data } = await api.get(`/events/${eventId}/comments`, {
+          params: { limit, offset },
+        });
+      
+        return Array.isArray(data) ? data : [];
+      }
+  
+      
+      async getAttendees(eventId: number) {
+        const { data } = await api.get(`/events/${eventId}/attendees`);
+        return data;
+      }
+    
+      async createComment(eventId: number, content: string) {
+        const { data } = await api.post(`/events/${eventId}/comments`, { content });
+        return data;
+      }
+    
+      async deleteComment(eventId: number, commentId: number) {
+        const { data } = await api.delete(
+          `/events/${eventId}/comments/${commentId}`
+        );
+        return data;
+      }
+
+    async createEvent(form: FormData) {
+        const { data } = await api.post("/events", form, {
+        headers: { "Content-Type": "multipart/form-data" },
+        });
+        return data;
+        }
+  
       
 }
 
